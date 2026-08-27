f1c2f04347ab150f85f6af3f2fcf0b2fe67bcf09
David Carboni <david@carboni.io>
2026-08-27 17:24:47 +0100

    Generate CDK infrastructure code

---
+++ .infrastructure/test/seed_cairn.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SeedCairn from '../lib/seed_cairn-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/seed_cairn-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SeedCairn.SeedCairnStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
