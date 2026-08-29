f6a06ff5144c3e05fcafa32c42d216d7506e9d3a
David Carboni <david@carboni.io>
2026-08-29 21:45:26 +0100

    Rename stack and set account ID

--- .infrastructure/bin/seedCairn.ts
+++ .infrastructure/bin/seedCairn.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib/core';
-import { SeedCairnStack } from '../lib/seed_cairn-stack';
+import { SeedCairnStack } from '../lib/seedCairn-stack';
 
 const app = new cdk.App();
 new SeedCairnStack(app, 'SeedCairnStack', {
@@ -14,7 +14,7 @@ new SeedCairnStack(app, 'SeedCairnStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: '224796772588', region: 'eu-west-2' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
